bb0f8e955559ef12fd1d8eb722584f9940afec08
Fennrii <axsateagle@gmail.com>
2024-12-02 11:29:07 -0500

    part 1 fix

--- 02/answer.py
+++ 02/answer.py
@@ -1,10 +1,11 @@
 def safety_check(line):
-    safe = True
     last=int(line[0])
     curr=int(line[1])
-    if curr>last and abs(curr-last)<4:
+    if abs(curr-last)>3:
+        return False
+    if curr>last:
         increase = True
-    elif curr<last and abs(curr-last)<4:
+    elif curr<last:
         increase = False
     else:
         return False
@@ -12,18 +13,26 @@ def safety_check(line):
     for i in range(len(line)-2):
         curr = int(line[i+2])
         if increase and curr<=last:
-            safe = False 
-        elif curr>=last:
-            safe = False
+            # print(f"Last: {last}, Curr: {curr}, increasing = {increase}")
+            return False 
+        elif not increase and curr>=last:
+            print(f"Last: {last}, Curr: {curr}, increasing = {increase}")
+            return False
         if abs(curr-last)>3:
-            safe = False
+            # print(f"Last: {last}, Curr: {curr}, increasing = {increase}")
+            return False
         last = curr
-    return safe
+    return True
 with open("input","r") as f:
     file = f.readlines()
 
 safety_list = []
 for line in file:
+    line = line.strip('\n')
     line_arry = line.split(' ')
-    safety_list.append(safety_check(line_arry))
+    # print(line_arry)
+    safety = safety_check(line_arry)
+    # print(safety)
+    safety_list.append(safety)
+
 print(sum(safety_list))
