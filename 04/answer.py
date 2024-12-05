f83ac70c77b36ec7ebf3c872cf0e1ac9b9ba0059
Fennrii <axsateagle@gmail.com>
2024-12-05 17:38:53 -0500

    Day 4 Solution

--- 04/answer.py
+++ 04/answer.py
@@ -1,70 +1,64 @@
 
-
-xmas = ['X','M','A','S']
-# print(word[0][1]='M')
-# print(word[1][0]='Z')
+xmas = "XMAS"
 
 def find_word(array,word,wordindx,row,col,direction):
+    rows = len(array)
+    cols = len(array[0])
     if wordindx >= len(word):
-        print("\nTrue")
+        # print("\nTrue")
         return 1
-    if row < 0 or col < 0:
-        print("\nFalse - out of bounds")
+
+    if (0 > row or row >= rows) or (0 > col or col >= cols):
+        # print(f"\nFalse - [{row}][{col}] out of bounds [{len(array)-1}][{len(array[0])-1}]")
         return 0
-    if row >= len(array) or col >= len(array[0]):
-        print("\nFalse - out of bounds")
-        return 0
-    row_offset = 0
-    col_offset = 0
-    match direction:
-        case 1:
-            row_offset = -1
-            col_offset = -1
-        case 2:
-            row_offset = -1
-        case 3:
-            row_offset = -1
-            col_offset = 1
-        case 4:
-            col_offset = -1
-        case 5:
-            col_offset = 1
-        case 6:
-            row_offset = 1
-            col_offset = -1
-        case 7:
-            row_offset = 1
-        case 8:
-            row_offset = 1
-            col_offset = 1
-        case _:
-            print(f"Direction {direction} not accepted")
-            exit()
+    # print(array[row][col],end='')
     if array[row][col] == word[wordindx]:
-        print(array[row][col],end='')
-        return find_word(array,word,wordindx+1,row+row_offset,col_offset+col_offset,direction)
-    print(f"\nFalse - {array[row][col]} != {word[wordindx]}")
+        return find_word(array,word,wordindx+1,row+direction[0],col+direction[1],direction)
+    # print(f"\nFalse - {array[row][col]} != {word[wordindx]}")
     return 0
 
 def search_all_dirs(array,word,row,col):
     result = 0
-    for _i in range(1,9):
-        result += find_word(array,word,0,row,col,_i)
+    directions = [
+            (1,0),(0,1),(-1,0),(0,-1),
+            (1,1),(1,-1),(-1,1),(-1,-1)
+            ]
+    for direction in directions:
+        result += find_word(array,word,0,row,col,direction)
     return result
 
 with open('input','r') as f:
-    lines = f.readlines()
+    lines = list(map(lambda n: n.strip('\n'),f.readlines()))
 
-
-for i in range(len(lines)):
-    lines[i] = lines[i].strip('\n')
 
 found_words = 0
 for i in range(len(lines)):
     for j in range(len(lines[i])):
         if lines[i][j] == xmas[0]:
+            # print(f"[{i}][{j}]")
             found_words += search_all_dirs(lines,xmas,i,j)
-            # print(lines[i][j])
-            
 
+
+print(found_words)
+mas = "MAS"
+def find_x_mas(array, word, row, col):
+    result = 0
+    directions = [
+            (1,1),(1,-1),(-1,-1),(-1,1)
+            ]
+    rev_directions = [
+            (-1,-1),(-1,1),(1,1),(1,-1)
+            ]
+    count = 0
+    for dirindx in range(len(directions)):
+        regdir = directions[dirindx]
+        invdir = rev_directions[dirindx]
+        count+= find_word(array,word,0,row+regdir[0],col+regdir[1],invdir)
+    if count == 2:
+        return 1
+    return 0
+found_words = 0
+for i in range(len(lines)):
+    for j in range(len(lines[0])):
+        found_words += find_x_mas(lines,mas,i,j)
 print(found_words)
