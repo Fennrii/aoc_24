103c24059c2d8c8a04177a9f8e5ba4caf03715d2
Fennrii <axsateagle@gmail.com>
2024-12-06 12:17:27 -0500

    Day 5 part 2

--- 05/answer.py
+++ 05/answer.py
@@ -33,8 +33,8 @@ for line in lines:
         indx += 1
 
 req_dict = defaultdict(list)
-# The order is for X|Y X cannot follow Y
-# Creates a dict of Y's that contain a list of X's
+# The order is for X|Y Y cannot follow X
+# Creates a dict of X's that contain a list of Y's
 for i in rules:
     tmp = i.split('|')
     req_dict[tmp[0]].append(tmp[1])
@@ -46,7 +46,40 @@ for i in prints:
     result_list.append(check_order(i,req_dict))
 
 result = 0
+incorrect_prints = []
 for i in range(len(result_list)):
     if result_list[i]:
         result += int(prints[i][int(len(prints[i])/2)])
+    else:
+        incorrect_prints.append(prints[i])
+print(result)
+
+
+def order_prints(order,rules):
+    sorted = False
+    orlen = len(order)
+    print(f"\n{order}")
+    while not sorted:
+        sort_made = False
+        for i in range(orlen): # Check each item in order
+            for j in range(i): # Check only next items
+                if check_rule(order[j], order[i], rules):
+                    print(f"FAILED\n{order[j]} is in {order[i]}'s list: {rules[order[i]]}")
+                    print(f"Swapping {order[j]} and {order[i]}")
+                    sorted =  False
+                    sort_made = True
+                    tmp = order[j]
+                    order[j] = order[i]
+                    order[i]=tmp
+                    break
+            if sort_made:
+                break
+        if not sort_made:
+            sorted = True
+    print(f"PASSED: Sorted list: {order}")
+    return order
+result = 0
+for i in range(len(incorrect_prints)):
+    incorrect_prints[i] = order_prints(incorrect_prints[i],req_dict)
+    result += int(incorrect_prints[i][int(len(incorrect_prints[i])/2)])
 print(result)
